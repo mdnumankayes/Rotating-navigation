d4b5d3a53ec1f9ec38f0ae42afa4d54e25a4fe61
mdnumankayes <mdnumankayes@gmail.com>
2021-02-20 16:28:42 +0600

    delete duplicate file

---
+++ day6/script.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll('.box')
+
+
+window.addEventListener('scroll',checkbox)
+checkbox()
+
+function checkbox(){
+    const triggerBottom = window.innerHeight/5*4
+
+    boxes.forEach(box =>{
+        const boxtop = box.getBoundingClientRect().top
+
+        if(boxtop < triggerBottom){
+            box.classList.add('show')
+        }
+        else{
+            box.classList.remove('show')
+        }
+    })
+}
