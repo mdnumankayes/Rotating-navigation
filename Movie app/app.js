881c6e226666beff856bb62ae31b37370e53ff1d
mdnumankayes <mdnumankayes@gmail.com>
2021-04-03 02:25:25 +0600

    udate overview div

--- Movie app/app.js
+++ Movie app/app.js
@@ -18,14 +18,15 @@ function showMovie(movies){
         const {title,poster_path,vote_average,overview } =movie
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
-        movieEl.innerHTML = ` <img src="${IMG_URL+poster_path}" alt="${title}">
-        <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
+        movieEl.innerHTML = `<div class="img_div"> <img src="${IMG_URL+poster_path}" alt="${title}">
         <div class="overview">
             <h3>Overview</h3>
               ${overview}
+        </div>
+        </div>
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>`
 
         main.appendChild(movieEl)
