2c04cafa910ea63d1e05a01962757deff3a2d30b
mdnumankayes <mdnumankayes@gmail.com>
2021-04-25 02:18:05 +0600

    add confirmation message with pass Length project

--- Password_Stregnth_Background/app.js
+++ Password_Stregnth_Background/app.js
@@ -1,19 +1,23 @@
 const password = document.getElementById('password')
 const background = document.getElementById('background')
 const submitBtn = document.getElementById('submit')
- 
+ let passlength = 0
 
 
 password.addEventListener('input', (e)=>{
     const input = e.target.value
-    const length = input.length
-
+    const length =  input.length
+     passlength = length
     const blurValue = (20 - length * 2)
     background.style.filter = `blur(${blurValue}px)`
     
-    
+
 })
 
 submitBtn.addEventListener('click',()=>{
-   alert('Successfully Submitted') 
+   alert('Successfully Submitted' +passlength) 
 })
+// function submitLoginForm(e){
+//     e.preventDefault();
+//   console.log(e)
+// }
