5752d0df6ec6d0f98c3ec7db327cb2a5e9c09534
mdnumankayes <mdnumankayes@gmail.com>
2021-04-30 23:56:33 +0600

    add random test Image project

---
+++ Rendom test Image/app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container')
+
+const unsplashUrl = 'https://source.unsplash.com/random/'
+
+const row = 10
+
+for(let i = 0; i < row * 3 ; i++){
+   const img =  document.createElement('img')
+   img.src = `${unsplashUrl}${getrandomSize()}`
+   
+   container.appendChild(img)
+  
+}
+function getrandomSize(){
+
+    return `${getRandomNmbr()}x${getRandomNmbr()}`
+}
+
+function getRandomNmbr(){
+    return Math.floor(Math.random() * 10) + 300
+    
+}
+
+console.log(getrandomSize())
